98b630ee35a5cfb88d27c6b44290fe954f7211ed
codingnguyen <codyng1999@gmail.com>
2021-01-31 00:27:20 -0600

    Deep Fryer

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,5 @@
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Backdrop from '@material-ui/core/Backdrop';
 
 function App() {
   return (
@@ -12,10 +10,6 @@ function App() {
           <h1 align="center">Howdy!</h1>
           </Typography>
         </AppBar>
-
-        
-        
-        <Grid align="center">Test1</Grid>
       </header>
     </div>
   );
